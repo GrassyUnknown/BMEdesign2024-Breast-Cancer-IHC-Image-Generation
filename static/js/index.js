ea83d83927a92bd508a17f12103a04f9a9e3e68f
Null <68577047+GrassyUnknown@users.noreply.github.com>
2024-07-16 00:06:20 +0800

    edit3

--- static/js/index.js
+++ static/js/index.js
@@ -3,7 +3,6 @@ window.HELP_IMPROVE_VIDEOJS = false;
 
 $(document).ready(function() {
     // Check for click events on the navbar burger icon
-
     var options = {
 			slidesToScroll: 1,
 			slidesToShow: 1,
@@ -20,6 +19,7 @@ $(document).ready(function() {
 
 $("#chooseImage").on('change',function(){
 	let filePath = $(this).val(); //获取到input的value，里面是文件的路径
+	
 	let fileFormat = filePath.substring(filePath.lastIndexOf(".")).toLowerCase(); //获取文件后缀
 
 	let src = window.URL.createObjectURL(this.files[0]); //转成可以在本地预览的格式
@@ -31,7 +31,22 @@ $("#chooseImage").on('change',function(){
 		return ;
 	}
 	$('#img-avatar').attr('src', src); //将图片地址放置在img的src中。
-	$('#img-avatar-2').attr('src', src); //将图片地址放置在img的src中。
-});
+	let fileName = filePath.substring(filePath.lastIndexOf('\\'));
 
+	let allFiles = ['\\00000_test_1+.png','\\00001_test_2+.png','\\00002_test_2+.png','\\00003_test_3+.png','\\00004_test_0.png','\\00005_test_1+.png','\\00006_test_3+.png','\\00007_test_1+.png','\\00008_test_3+.png','\\00009_test_2+.png','\\00010_test_1+.png','\\00011_test_3+.png','\\00012_test_1+.png','\\00013_test_0.png','\\00014_test_2+.png','\\00015_test_1+.png','\\00016_test_1+.png','\\00017_test_1+.png','\\00018_test_2+.png','\\00019_test_2+.png','\\00020_test_1+.png']
+	for(let i = 0; i < allFiles.length; i++){
+		if(allFiles[i] === fileName){
+			let src2 = './static/images/result' + fileName;
+			$('#img-avatar-2').attr('src', src2); //将图片地址放置在img的src中。
+			return ;
+		}
+	}
+	
+	$('#img-avatar-2').attr('src', './static/images/result/error.png'); //将图片地址放置在img的src中。
+	return ;
+
+	
+});
 })
+
+
